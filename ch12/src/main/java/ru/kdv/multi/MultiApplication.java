85be97a0d48488833956bb72d452d81d5b3af01c
KDanila <concista@gmail.com>
2020-12-04 11:24:30 +0300

    init module

---
+++ ch12/src/main/java/ru/kdv/multi/MultiApplication.java
@@ -0,0 +1,13 @@
+package main.java.ru.kdv.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiApplication.class, args);
+	}
+
+}
